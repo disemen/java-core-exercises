4d8a4c3b1ef09e58cb95d358c9d5f70859220a3e
dima <disemen@gmail.com>
2020-11-09 09:13:59 +0200

    crazy optionals completed

--- crazy-optionals/src/main/java/com/bobocode/CrazyOptionals.java
+++ crazy-optionals/src/main/java/com/bobocode/CrazyOptionals.java
@@ -11,9 +11,7 @@ import com.bobocode.model.CreditAccount;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalDouble;
+import java.util.*;
 
 public class CrazyOptionals {
 
@@ -24,7 +22,8 @@ public class CrazyOptionals {
      * @return optional object that holds text
      */
     public static Optional<String> optionalOfString(@Nullable String text) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return Optional.ofNullable(text);
     }
 
     /**
@@ -34,7 +33,9 @@ public class CrazyOptionals {
      * @param amount          money to deposit
      */
     public static void deposit(AccountProvider accountProvider, BigDecimal amount) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        accountProvider.getAccount().
+                ifPresent(account -> account.setBalance(account.getBalance().add(amount)));
     }
 
     /**
@@ -44,7 +45,8 @@ public class CrazyOptionals {
      * @return optional object that holds account
      */
     public static Optional<Account> optionalOfAccount(@Nonnull Account account) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return Optional.of(account);
     }
 
     /**
@@ -56,7 +58,9 @@ public class CrazyOptionals {
      * @return account from provider or defaultAccount
      */
     public static Account getAccount(AccountProvider accountProvider, Account defaultAccount) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accountProvider.getAccount()
+                .orElse(defaultAccount);
     }
 
     /**
@@ -67,7 +71,9 @@ public class CrazyOptionals {
      * @param accountService
      */
     public static void processAccount(AccountProvider accountProvider, AccountService accountService) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        accountProvider.getAccount()
+                .ifPresentOrElse(accountService::processAccount, accountService::processWithNoAccount);
     }
 
     /**
@@ -78,7 +84,10 @@ public class CrazyOptionals {
      * @return provided or generated account
      */
     public static Account getOrGenerateAccount(AccountProvider accountProvider) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+//        return accountProvider.getAccount().orElse(Accounts.generateAccount());
+        return accountProvider.getAccount()
+                .orElseGet(Accounts::generateAccount);
     }
 
     /**
@@ -88,7 +97,9 @@ public class CrazyOptionals {
      * @return optional balance
      */
     public static Optional<BigDecimal> retrieveBalance(AccountProvider accountProvider) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accountProvider.getAccount()
+                .map(Account::getBalance);
     }
 
     /**
@@ -99,7 +110,9 @@ public class CrazyOptionals {
      * @return provided account
      */
     public static Account getAccount(AccountProvider accountProvider) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accountProvider.getAccount()
+                .orElseThrow(() -> new AccountNotFoundException("No Account provided!"));
     }
 
     /**
@@ -109,7 +122,9 @@ public class CrazyOptionals {
      * @return optional credit balance
      */
     public static Optional<BigDecimal> retrieveCreditBalance(CreditAccountProvider accountProvider) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accountProvider.getAccount()
+                .flatMap(CreditAccount::getCreditBalance);
     }
 
 
@@ -121,7 +136,11 @@ public class CrazyOptionals {
      * @return optional gmail account
      */
     public static Optional<Account> retrieveAccountGmail(AccountProvider accountProvider) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accountProvider.getAccount()
+                .filter(account -> account.getEmail().split("@")[1].equals("gmail.com"))
+//                .or(Optional::empty)
+                ;
     }
 
     /**
@@ -134,7 +153,10 @@ public class CrazyOptionals {
      * @return account got from either accountProvider or fallbackProvider
      */
     public static Account getAccountWithFallback(AccountProvider accountProvider, AccountProvider fallbackProvider) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accountProvider.getAccount()
+                .or(fallbackProvider::getAccount)
+                .orElseThrow();
     }
 
     /**
@@ -145,7 +167,10 @@ public class CrazyOptionals {
      * @return account with the highest balance
      */
     public static Account getAccountWithMaxBalance(List<Account> accounts) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accounts.stream()
+                .max(Comparator.comparing(Account::getBalance))
+                .orElseThrow(NoSuchElementException::new);
     }
 
     /**
@@ -155,7 +180,11 @@ public class CrazyOptionals {
      * @return the lowest balance values
      */
     public static OptionalDouble findMinBalanceValue(List<Account> accounts) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accounts.stream()
+                .map(Account::getBalance)
+                .mapToDouble(BigDecimal::doubleValue)
+                .min();
     }
 
     /**
@@ -165,7 +194,13 @@ public class CrazyOptionals {
      * @param accountService
      */
     public static void processAccountWithMaxBalance(List<Account> accounts, AccountService accountService) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+//        accountService.processAccount(accounts.stream()
+//                .max(Comparator.comparing(Account::getBalance))
+//                .get());
+        accounts.stream()
+                .max(Comparator.comparing(Account::getBalance))
+                .ifPresent(accountService::processAccount);
     }
 
     /**
@@ -175,7 +210,12 @@ public class CrazyOptionals {
      * @return total credit balance
      */
     public static double calculateTotalCreditBalance(List<CreditAccount> accounts) {
-        throw new UnsupportedOperationException("Some people say that method does not work until you implement it");
+        // todo: done
+        return accounts.stream()
+                .map(CreditAccount::getCreditBalance)
+                .flatMap(Optional::stream)
+                .mapToDouble(BigDecimal::doubleValue)
+                .sum();
     }
 }
 
