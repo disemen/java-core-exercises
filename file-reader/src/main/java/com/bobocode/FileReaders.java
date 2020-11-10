78045872a4209a8a8a904d72235bdd6a1bfec1a8
dima <disemen@gmail.com>
2020-11-10 18:59:14 +0200

    file readers completed

--- file-reader/src/main/java/com/bobocode/FileReaders.java
+++ file-reader/src/main/java/com/bobocode/FileReaders.java
@@ -1,5 +1,15 @@
 package com.bobocode;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 /**
  * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
  */
@@ -12,6 +22,18 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        //todo: done
+        try {
+            Objects.requireNonNull(fileName);
+            URL fileUrl = FileReaders.class.getClassLoader().getResource(fileName);
+            Path path = Paths.get(Objects.requireNonNull(fileUrl).toURI());
+
+//            return Files.lines(path).collect(Collectors.joining("\n"));
+            return String.join("\n", Files.readAllLines(path));
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
